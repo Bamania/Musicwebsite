fd545c4236602f3ca039cd52a19d2a79b1b9cd0b
Baman Irani <amany29074@gmail.com>
2024-03-29 01:31:26 +0530

    completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import React,{useState} from 'react'
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
+import Link from 'next/link';
 
 function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
@@ -9,28 +10,25 @@ function Navbar({ className }: { className?: string }) {
     <div
     className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}  
     > <Menu setActive={setActive}>
-    <MenuItem setActive={setActive} active={active} item="Home">
-      <div className="flex flex-col space-y-4 text-sm">
-        
-      </div>
-    </MenuItem>
+      <Link href={"/"}>
+            <MenuItem setActive={setActive} active={active} item="Home">
+            
+            </MenuItem>
+            </Link>
+
     <MenuItem setActive={setActive} active={active} item="Our Courses">
       <div className="  text-sm flex flex-col gap-4 p-4">
-        <HoveredLink href="/hobby">All Courses</HoveredLink>
-        <HoveredLink href="/individual">Basic Music Theory</HoveredLink>
-        <HoveredLink href="/team">Advanced Composition</HoveredLink>
-        <HoveredLink href="/enterprise">Songwriting</HoveredLink>
-        <HoveredLink href="/enterprise">Music Production</HoveredLink>
+        <HoveredLink href="/courses">All Courses</HoveredLink>
+        <HoveredLink href="/">Basic Music Theory</HoveredLink>
+        <HoveredLink href="">Advanced Composition</HoveredLink>
+        <HoveredLink href="/">Songwriting</HoveredLink>
+        <HoveredLink href="/">Music Production</HoveredLink>
       </div>
     </MenuItem>
-    <MenuItem setActive={setActive} active={active} item="Pricing">
-      <div className="flex flex-col space-y-4 text-sm">
-      <HoveredLink href="/web-dev">Web Development</HoveredLink>
-        <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-        <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-        <HoveredLink href="/branding">Branding</HoveredLink>
-      </div>
-    </MenuItem>
+    <Link href={"/contact"}>
+    <MenuItem setActive={setActive} active={active} item="Contact us">
+     </MenuItem>
+     </Link>
   </Menu></div>
   )
 }
